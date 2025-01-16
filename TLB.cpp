01c218bc05b02bf066e3c8cdb23d04598c400338
Rafael Pascoali Czerniej <138595495+RafaelPasCz@users.noreply.github.com>
2025-01-16 14:18:05 -0300

    Add files via upload

--- TLB.cpp
+++ TLB.cpp
@@ -5,22 +5,32 @@ using namespace std;
 #include <queue>
 
 void AbrirArquivo(const string &nomeArquivo) {
-    int NumPos;
-    int hits = 0;
-    int misses = 0;
+    int numPosDado;
+    int numPosInstr;
+    int instrHits = 0;
+    int instrMisses = 0;
+    int dadoHits = 0;
+    int dadoMisses = 0;
 
-    cout << "Digite o numero de entradas na TLB: ";
-    cin >> NumPos;
+    cout << "Digite o numero de entradas na TLB de instruções";
+    cin >> numPosInstr;
+    cout << "Digite o numero de entradas na TLB de paginas: ";
+    cin >> numPosDado;
 
     // TLB usando um vetor para armazenar os valores
-    string TLBentries[NumPos];
-    for (int i = 0; i < NumPos; i++) {
-        TLBentries[i] = "";  // Inicializa a TLB com valores vazios
+    string TLBDadoEntries[numPosDado];
+    string TLBInstrEntries[numPosInstr];
+
+    for (int i = 0; i < numPosDado; i++) {
+        TLBDadoEntries[i] = "";  // Inicializa a TLB com valores vazios
     }
-
+    
+    for (int i = 0; i < numPosInstr; i++){
+        TLBInstrEntries[i] = "";
+    }
     // Fila para controlar a ordem de inserção (FIFO)
-    queue<int> fifoQueue;
-
+    queue<int> fifoQueueDado;
+    queue<char> fifoQueueInstr;
     ifstream Arquivo(nomeArquivo);
 
     if (!Arquivo) {
@@ -30,48 +40,92 @@ void AbrirArquivo(const string &nomeArquivo) {
 
     string linha;
     while (getline(Arquivo, linha)) {
-        bool encontrado = false;
+        bool dadoEncontrado = false;
+        bool instrEncontrado = false;
+        string op = linha.substr(0, 1);
+        string linhaPag = linha.substr(1);
 
-        // Verifica se a linha já está na TLB
-        for (int j = 0; j < NumPos; j++) {
-            if (TLBentries[j] == linha) {  // Verifica se a linha está na TLB
-                hits++;
-                encontrado = true;
+        if(op == "I") {
+        // Verifica se a instrução já está na TLB
+        for (int j = 0; j < numPosInstr; j++) {
+            if (TLBInstrEntries[j] == linhaPag) {  // Verifica se está na TLB
+                instrHits++;
+                instrEncontrado = true;
                 break;
             }
         }
 
-        if (!encontrado) {  // Se não encontrou a linha na TLB, é um miss
-            misses++;
+        if (!instrEncontrado) {  // Se não encontrou na TLB, é um miss
+            instrMisses++;
 
             // Se a TLB está cheia, remove o elemento mais antigo (FIFO)
-            if (fifoQueue.size() == NumPos) {
-                int idx = fifoQueue.front();  // Pega o índice do item mais antigo
-                fifoQueue.pop();  // Remove da fila
+            if (fifoQueueInstr.size() == numPosInstr) {
+                int idx = fifoQueueInstr.front();  // Pega o índice do item mais antigo
+                fifoQueueInstr.pop();  // Remove da fila
 
                 // Substitui o valor na TLB
-                TLBentries[idx] = linha;
+                TLBInstrEntries[idx] = linhaPag;
             } else {
                 // Se a TLB não está cheia, encontra a próxima posição livre
-                for (int j = 0; j < NumPos; j++) {
-                    if (TLBentries[j] == "") {  // Encontra uma posição vazia
-                        TLBentries[j] = linha;
-                        fifoQueue.push(j);  // Adiciona o índice à fila FIFO
+                for (int j = 0; j < numPosInstr; j++) {
+                    if (TLBInstrEntries[j] == "") {  // Encontra uma posição vazia
+                        TLBInstrEntries[j] = linhaPag;
+                        fifoQueueInstr.push(j);  // Adiciona o índice à fila FIFO
                         break;
                     }
                 }
             }
         }
+        
     }
+/*________________________________________________________________________________________________________________________*/
+     else{
 
+        // Verifica se a linha já está na TLB
+        for (int j = 0; j < numPosDado; j++) {
+            if (TLBDadoEntries[j] == linhaPag) {  // Verifica se a linha está na TLB
+                dadoHits++;
+                dadoEncontrado = true;
+                break;
+            }
+        }
+
+            if (!dadoEncontrado) {  // Se não encontrou a linha na TLB, é um miss
+                dadoMisses++;
+
+                // Se a TLB está cheia, remove o elemento mais antigo (FIFO)
+                if (fifoQueueDado.size() == numPosDado) {
+                    int idx = fifoQueueDado.front();  // Pega o índice do item mais antigo
+                    fifoQueueDado.pop();  // Remove da fila
+    
+                    // Substitui o valor na TLB
+                    TLBDadoEntries[idx] = linhaPag;
+                } else {
+                    // Se a TLB não está cheia, encontra a próxima posição livre
+                    for (int j = 0; j < numPosDado; j++) {
+                        if (TLBDadoEntries[j] == "") {  // Encontra uma posição vazia
+                            TLBDadoEntries[j] = linhaPag;
+                            fifoQueueDado.push(j);  // Adiciona o índice à fila FIFO
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
     Arquivo.close();
 
     // Calculando a taxa de acertos
-    float hitRate = (float)hits / (hits + misses);
+    float dadoHitRate = (float)dadoHits / (dadoHits + dadoMisses);
+    float instrHitRate = (float)instrHits / (instrHits + instrMisses);
 
-    cout << "Hits: " << hits << endl;
-    cout << "Misses: " << misses << endl;
-    cout << "Hit Rate: " << hitRate * 100 << "%" << endl;  // Exibe a taxa de acertos em porcentagem
+    cout << "instrHits: " << instrHits << endl;
+    cout << "instrMisses: " << instrMisses << endl;
+    cout << "instr Hit Rate: " << instrHitRate * 100 << "%" << endl;
+
+    cout << "pagHits: " << dadoHits << endl;
+    cout << "pagMisses: " << dadoMisses << endl;
+    cout << "pag Hit Rate: " << dadoHitRate * 100 << "%" << endl;  // Exibe a taxa de acertos em porcentagem
 }
 
 int main() {
